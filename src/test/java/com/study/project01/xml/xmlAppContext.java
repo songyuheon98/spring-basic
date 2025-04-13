1d1985ec6136a24342e1c45d5ed6e9e5f16494ee
songyuheon98 <songyuheon2750@gmail.com>
2025-04-13 13:17:56 +0900

    Spring Container And Bean

---
+++ src/test/java/com/study/project01/xml/xmlAppContext.java
@@ -0,0 +1,16 @@
+package com.study.project01.xml;
+
+import com.study.project01.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class xmlAppContext {
+
+    @Test
+    void xmlAppContext(){
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
